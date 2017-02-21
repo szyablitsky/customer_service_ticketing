696fa9cf9ac453ef98a9992012c65be466bf78a1
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2017-02-21 22:46:42 +1000

    sign out controller

---
+++ app/concepts/session/destroy.rb
@@ -0,0 +1,10 @@
+class Session::Destroy < ApplicationOperation
+  def model!(params)
+    return unless params[:current_user]
+    params[:current_user].authentications.where(key: params['key']).first
+  end
+
+  def process(*)
+    model.destroy if model.present?
+  end
+end
