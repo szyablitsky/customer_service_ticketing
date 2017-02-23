ee0114d89c4ac0ecbfb69ee8d5205cd7e6f652c5
Sergey Zyablitsky <sergey.zyablitsky@gmail.com>
2017-02-23 14:30:13 +1000

    requests filtering

--- client/app/selectors/requests.js
+++ client/app/selectors/requests.js
@@ -1,1 +1,24 @@
-export const filteredRequestIds = (state) => state.requests.ids
+import { createSelector } from 'reselect'
+import filter from 'lodash/filter'
+
+const getIds = (state) => state.requests.ids
+const getRequestsFilter = (state) => state.requests.filter
+const getRequests = (state) => state.entities.requests
+
+export const filteredRequestIds = createSelector(
+  getIds, getRequestsFilter, getRequests,
+  (ids, requestsFilter, requests) =>
+    filter(ids, (id) => {
+      const request = requests[id]
+      switch (requestsFilter) {
+        case 'open': return request.open
+        case 'closed': return !request.open
+        case 'unassigned': return request.supportAgentId === null
+        case 'assigned-open':
+          return request.supportAgentId !== null && request.open
+        case 'assigned-closed':
+          return request.supportAgentId !== null && !request.open
+        default: return false
+      }
+    })
+)
